229da5cd82ae9d9edc68158db4faa7614aa80d0a
Kristian Dotson <KDotson387@gmail.com>
2023-06-06 09:58:47 -0500

    test trial for auth0

--- server.js
+++ server.js
@@ -5,17 +5,35 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose')
 // process.env.DatabaseUrl = 'mongodb://localhost:27017/BookDB'
-const BookData = require('./books'); // Import the BookData model or data access layer
 
+// const jwks = require('jwks-rsa')
+const axios = require('axios')
+// const jwt = require('express-jwt')
+// const { JwksClient } = require('jwks-rsa');
+const BookData = require('./books'); // Import the BookData model or data access layer
 const Seed = require('./seed'); // Import the seed script for database population
+
+
+
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 const PORT = process.env.PORT || 3001;
 
+// const verifyJWT = jwt({
+//   secret: jwks.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: 'http://allow-edit-book.us.auth0.com/.well-known/jwks.json'
+//   }),
+//   audience: 'http://localhost:3001',
+//   issuer: 'http://allow-edit-book.us.auth0.com/',
+//   algorithms: ['RS256']
+// }).unless({ path: ['/books'] })
 
-
+// app.use(verifyJWT);
 
 app.get('/', (req, res) => {
   // Sending a response to the client
@@ -24,13 +42,22 @@ app.get('/', (req, res) => {
 
 })
 
-app.get('/books', async (req, res) => {
+app.get('/books',  async (req, res) => {
   try {
     await mongoose.connect(process.env.Database_Url, {
       useNewUrlParser: true,
       useUnifiedTopology: true
 
     });
+    // const accessToken = request.headers.authorization.split(' ')[1];
+    // const user = await axios.get('http://allow-edit-book.us.auth0.com/userinfo', {
+    //   headers: {
+    //     authorization: `Bearer ${accessToken}`
+    //   }
+    // });
+    // const userinfo = user.data;
+    // res.send(userinfo)
+
     //const conn = await mongoose.createConnection(process.env.Database_Url).asPromise();
     //mongoose.model('Book', BookData)
 
@@ -54,6 +81,17 @@ app.post('/books', async (req, res) => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
+
+    // const accessToken = request.headers.authorization.split(' ')[1];
+    // const user = await axios.get('http://allow-edit-book.us.auth0.com/userinfo', {
+    //   headers: {
+    //     authorization: `Bearer ${accessToken}`
+    //   }
+    // });
+    // const userinfo = user.data;
+    // res.send(userinfo)
+    
+    
     // Create a new book record in the database using the data from the request body
     const newBook = await BookData.create(req.body);
     const books = await BookData.find()
@@ -65,7 +103,7 @@ app.post('/books', async (req, res) => {
 });
 
 
-app.delete('/books/:id', async (req, res) => {
+app.delete('/books/:id',  async (req, res) => {
   try {
     await mongoose.connect(process.env.Database_Url, {
       useNewUrlParser: true,
@@ -73,7 +111,16 @@ app.delete('/books/:id', async (req, res) => {
     });
 
     const bookId = req.params.id; // Access the book id from request params
-    
+
+    // const accessToken = request.headers.authorization.split(' ')[1];
+    // const user = await axios.get('http://allow-edit-book.us.auth0.com/userinfo', {
+    //   headers: {
+    //     authorization: `Bearer ${accessToken}`
+    //   }
+    // });
+    // const userinfo = user.data;
+    // res.send(userinfo)
+
     // Find and delete the book with the given id
     const deletedBook = await BookData.findByIdAndDelete(bookId);
     const books = await BookData.find()
@@ -87,11 +134,11 @@ app.delete('/books/:id', async (req, res) => {
     res.json(books); // Return the deleted book as a JSON response
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).send('Internal server error' );
+    res.status(500).send('Internal server error');
   }
 });
 
-app.put('/books/:id', async (req, res) => {
+app.put('/books/:id',  async (req, res) => {
   try {
     await mongoose.connect(process.env.Database_Url, {
       useNewUrlParser: true,
@@ -99,9 +146,20 @@ app.put('/books/:id', async (req, res) => {
     });
 
     const bookId = req.params.id; // Access the book id from request params
-    const {title, description, status} = req.body
+    const { title, description, status } = req.body
+
+    // const accessToken = request.headers.authorization.split(' ')[1];
+    // const user = await axios.get('http://allow-edit-book.us.auth0.com/userinfo', {
+    //   headers: {
+    //     authorization: `Bearer ${accessToken}`
+    //   }
+    // });
+    // const userinfo = user.data;
+    // res.send(userinfo)
+
+    
     // Find the book with the given id and update its data with the request body
-    const updatedBook = await BookData.findByIdAndUpdate(bookId,{title,description,status}, { new: true });
+    const updatedBook = await BookData.findByIdAndUpdate(bookId, { title, description, status }, { new: true });
     const books = await BookData.find();
 
     mongoose.disconnect(); // Disconnect from the database
@@ -118,30 +176,41 @@ app.put('/books/:id', async (req, res) => {
 });
 
 
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
 
 
-// app.get('/books', async (req, res) => {
-//   await mongoose.connect(process.env.DatabaseUrl)
-//   .then(()=> {
-//     console.log('is connected');
-//   })
-//   .catch((err) => {
-//     console.error('Error: ', err)
-//   })
-// mongoose.disconnect();
-//   res.send('it works hooray')
-// }
-
-// )
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || 'Internal server error';
+  res.status(status).send(message);
+});
 
 
-
-
-
-app.get('/test', (request, response) => {
+app.get('/test', async (request, response) => {
   // sending a response to the client
   response.send('test request received')
 
-})
+  // const accessToken = request.headers.authorization.split(' ')[1];
+  // try{
+  //   const user = await axios.get('http://allow-edit-book.us.auth0.com/userinfo', {
+  //     headers: {
+  //       authorization: `Bearer ${accessToken}`
+  //     }
+  //   });
+  //   const userinfo = user.data;
+  //   res.send(userinfo)
+
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).send('Internal Server Error')
+  // }
+}
+
+
+)
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`)); // Start the server and log the port
